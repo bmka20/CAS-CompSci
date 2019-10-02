aa70d6e79dab9e015364a908b1d01d1d5b44e580
bmka20 <54858592+bmka20@users.noreply.github.com>
2019-10-02 09:10:55 -0700

    Add files via upload

---
+++ Image Test/Image Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Test
+//
+//  Created by Brandon Adiele on 10/2/19.
+//  Copyright © 2019 Brandon Adiele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Image Test/Image TestTests/Image_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Image_TestTests.swift
+//  Image TestTests
+//
+//  Created by Brandon Adiele on 10/2/19.
+//  Copyright © 2019 Brandon Adiele. All rights reserved.
+//
+
+import XCTest
+@testable import Image_Test
+
+class Image_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
