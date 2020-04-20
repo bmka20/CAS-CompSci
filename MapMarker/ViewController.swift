a5d0c379cedb47262784b3b9f97e891b8e5cb2c8
bmka20 <54858592+bmka20@users.noreply.github.com>
2020-04-19 21:22:32 -0700

    Add files via upload

--- MapMarker/ViewController.swift
+++ MapMarker/ViewController.swift
@@ -9,10 +9,17 @@
 import MapKit
 import UIKit
 
-class ViewController: UIViewController, MKMapViewDelegate {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
     @IBOutlet weak var crossroadsLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    
+    var Lat = 34.029380
+    var Long = -118.351640
+    
+    var place:String = ""
     
     let CrossroadsLat = 34.0240892
     let CrossroadsLong = -118.4747321
@@ -25,27 +32,37 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        place = "void"
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view.
         mapView.delegate = self
-        
+        let point = MKMapPoint(CLLocationCoordinate2D(latitude: Lat, longitude: Long))
+        let size = MKMapSize(width: 1000, height: 1000)
+        let rect = MKMapRect(origin: point, size: size)
+        mapView.setRegion(MKCoordinateRegion(rect), animated: true)
     }
-
+    
     @IBAction func crossroadsLocation(_ sender: Any) {
-    let point = MKMapPoint(CLLocationCoordinate2D(latitude: CrossroadsLat, longitude: CrossroadsLong))
-    let size = MKMapSize(width: 1000, height: 1000)
-    let rect = MKMapRect(origin: point, size: size)
-    mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        place = "crossroads"
+        let point = MKMapPoint(CLLocationCoordinate2D(latitude: CrossroadsLat, longitude: CrossroadsLong))
+        let size = MKMapSize(width: 1000, height: 1000)
+        let rect = MKMapRect(origin: point, size: size)
+        mapView.setRegion(MKCoordinateRegion(rect), animated: true)
     }
     
     @IBAction func mountFujiLocation(_ sender: Any) {
-   let point = MKMapPoint(CLLocationCoordinate2D(latitude: MountFujiLat, longitude: MountFujiLong))
-      let size = MKMapSize(width: 1000, height: 1000)
+        place = "Mount Fuji"
+        let point = MKMapPoint(CLLocationCoordinate2D(latitude: MountFujiLat, longitude: MountFujiLong))
+        let size = MKMapSize(width: 1000, height: 1000)
         let rect = MKMapRect(origin: point, size: size);    mapView.setRegion(MKCoordinateRegion(rect), animated: true)
     }
     
     @IBAction func upennLocation(_ sender: Any) {
- let point = MKMapPoint(CLLocationCoordinate2D(latitude: UPennLat, longitude: UpennLong))
-    let size = MKMapSize(width: 1000, height: 1000)
+        place = "UPenn"
+        let point = MKMapPoint(CLLocationCoordinate2D(latitude: UPennLat, longitude: UpennLong))
+        let size = MKMapSize(width: 1000, height: 1000)
         let rect = MKMapRect(origin: point, size: size);    mapView.setRegion(MKCoordinateRegion(rect), animated: true)
     }
     
@@ -59,7 +76,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
     
     func Zoom(_ exp: Double) {
-        let factor = pow(2, exp)
+        /*
+         // Uses MKMapRect to create and set region
+         let factor = pow(2, exp)
+         let rect = mapView.visibleMapRect
+         let newSize = MKMapSize(width: rect.width * factor, height: rect.height * factor)
+         let newRect = MKMapRect(origin: rect.origin, size: newSize)
+         mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+         */
+        // Uses MKCoordinateSpan to create and set region
         let region = mapView.region
         let newLat = region.span.latitudeDelta * pow(2, exp)
         let newLong = region.span.longitudeDelta * pow(2, exp)
@@ -68,30 +93,52 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.setRegion(newRegion, animated: true)
     }
     
+    @IBAction func currentPosition(_ sender: Any) {
+        locationManager.requestLocation()
+    }
+    
     func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         
         let rect = mapView.visibleMapRect
         
         let crossroadsLocation = CLLocationCoordinate2D(latitude: CrossroadsLat, longitude: CrossroadsLong)
         let crossroadsPoint = MKMapPoint(crossroadsLocation)
-       
+        
         let mountFujiLocation = CLLocationCoordinate2D(latitude: MountFujiLat, longitude: MountFujiLong)
         let mountFujiPoint = MKMapPoint(mountFujiLocation)
-       
+        
         let upennLocation = CLLocationCoordinate2D(latitude: UPennLat, longitude: UpennLong)
         let upennPoint = MKMapPoint(upennLocation)
-       
+        
+        let homeLocation = CLLocationCoordinate2D(latitude: Lat, longitude: Long)
+        let currentPoint = MKMapPoint(homeLocation)
+        
         if rect.contains(crossroadsPoint) {
             crossroadsLabel.text = "Let's go Roads!"
         } else if rect.contains(mountFujiPoint){
             crossroadsLabel.text = "Mount Fuji!"
         } else if rect.contains(upennPoint){
             crossroadsLabel.text = "Let's go Quakers!"
+        } else if rect.contains(currentPoint){
+            crossroadsLabel.text = "That's Brandon's home!"
         } else {
-            crossroadsLabel.text = " "
+            crossroadsLabel.text = "You're not at a relevant place"
         }
-    
         
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let span = mapView.region.span
+        let coords = locations.suffix(1)[0].coordinate
+        let newRegion = MKCoordinateRegion(center: coords, span: span)
+        mapView.setRegion(newRegion, animated: true)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
 }
+
 
