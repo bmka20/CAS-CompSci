ecac9723c54fae426932fa2e5375bc34c7188ecb
bmka20 <54858592+bmka20@users.noreply.github.com>
2020-04-19 22:34:47 -0700

    Add files via upload

---
+++ Snake/GameManager.swift
@@ -0,0 +1,40 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by Brandon Adiele on 4/19/20.
+//  Copyright © 2020 Brandon Adiele. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    var scene: GameScene!
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+    
+    func initGame() {
+        //starting player position
+        scene.playerPositions.append((10, 10))
+        scene.playerPositions.append((10, 11))
+        scene.playerPositions.append((10, 12))
+        renderChange()
+    }
+    
+    func renderChange() {
+        for (node, x, y) in scene.gameArray {
+            if contains(a: scene.playerPositions, v: (x,y)) {
+                node.fillColor = SKColor.cyan
+            } else {
+                node.fillColor = SKColor.clear
+            }
+        }
+    }
+    
+    func contains(a:[(Int, Int)], v:(Int,Int)) -> Bool {
+        let (c1, c2) = v
+        for (v1, v2) in a { if v1 == c1 && v2 == c2 { return true } }
+        return false
+    }
+}
